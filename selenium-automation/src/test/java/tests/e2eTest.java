c3616b54b9723376e1f30ef2fd109fc24c677112
seongbae <seongbaelee94@gmail.com>
2025-04-14 16:53:23 -0400

    user login

--- selenium-automation/src/test/java/tests/e2eTest.java
+++ selenium-automation/src/test/java/tests/e2eTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,5 +9,13 @@ public class e2eTest {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/client");
+		driver.manage().window().maximize();
+		
+		
+		driver.findElement(By.id("userEmail")).sendKeys("qwe123@daum.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Qwe123!@");
+		driver.findElement(By.id("login")).click();
+		
+		
 	}
 }
