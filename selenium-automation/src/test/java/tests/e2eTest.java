e740fe3650e351797351f17127ca75fc58e4fda0
seongbae <seongbaelee94@gmail.com>
2025-04-15 17:14:05 -0400

    complete checkout process

--- selenium-automation/src/test/java/tests/e2eTest.java
+++ selenium-automation/src/test/java/tests/e2eTest.java
@@ -64,6 +64,15 @@ public class e2eTest {
 		Assert.assertTrue(sortedItems.equals(productNames));
 		driver.findElement(By.cssSelector(".subtotal button")).click();
 		
+		String targetCountry = "Canada";
+		WebElement countryBtnEle = driver.findElement(By.cssSelector(".user__address input"));
+		countryBtnEle.sendKeys("can");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".list-group")));
+		driver.findElement(By.xpath("//span[contains(text() , '" + targetCountry + "')]")).click();
+		String selectedCountry = countryBtnEle.getDomProperty("value");
+		Assert.assertTrue(selectedCountry.equalsIgnoreCase(targetCountry));
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
 //		driver.quit();
 		
 		
