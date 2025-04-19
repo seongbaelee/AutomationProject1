c3cac619288e0b31822ac494f263120fbc5c61f9
seongbae <seongbaelee94@gmail.com>
2025-04-19 18:18:31 -0400

    modification on initializeDriver method for various driver run

--- selenium-automation/src/test/java/com/automation/selenium_automation/helpers/Base.java
+++ selenium-automation/src/test/java/com/automation/selenium_automation/helpers/Base.java
@@ -1,46 +1,68 @@
 package com.automation.selenium_automation.helpers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.automation.selenium_automation.pages.LoginPage;
-import com.automation.selenium_automation.utils.Utils;
 
 public class Base {
 	public WebDriver driver;
-	public LoginPage loginPage;	
-	
-	public WebDriver initializeDriver() {
-		String browserName = "Chrome";
-		if (browserName == "Chrome") {
+	public LoginPage loginPage;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+				+ "\\src\\main\\java\\com\\automation\\selenium_automation\\resources\\GlobalData.properties");
+		prop.load(fis);
+		String browserName = System.getProperty("browser") != null ? System.getProperty("browser")
+				: prop.getProperty("browser");
+
+//		String browserName = "Chrome";
+//		if (browserName == "Chrome") {
+//			driver = new ChromeDriver();
+//		}
+//
+//		if (browserName == "firefox") {
+//			driver = new FirefoxDriver();
+//		}
+//
+//		if (browserName == "edge") {
+//			driver = new EdgeDriver();
+//		}
+
+		if (browserName.equalsIgnoreCase("Chrome")) {
 			driver = new ChromeDriver();
 		}
 
-		if (browserName == "firefox") {
+		if (browserName.equalsIgnoreCase("Firefox")) {
 			driver = new FirefoxDriver();
 		}
 
-		if (browserName == "edge") {
+		if (browserName.equalsIgnoreCase("Edge")) {
 			driver = new EdgeDriver();
 		}
-		
+
 		return driver;
 	}
-	
-	@BeforeMethod(alwaysRun=true)
-	public LoginPage launchApp() {
+
+	@BeforeMethod(alwaysRun = true)
+	public LoginPage launchApp() throws IOException {
 		driver = initializeDriver();
 		loginPage = new LoginPage(driver);
 		loginPage.gotoLoginPage();
 		return loginPage;
 	}
-	
-	@AfterMethod(alwaysRun=true)
+
+	@AfterMethod(alwaysRun = true)
 	public void quitWebsite() {
 		driver.quit();
 	}
